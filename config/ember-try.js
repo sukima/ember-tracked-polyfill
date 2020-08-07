0ceebaf3dc5861701e6f966c6fe0fb700d20eeef
Devin Weaver <suki@tritarget.org>
2020-08-06 21:21:53 -0400

    Configure supported versions

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,6 +7,14 @@ module.exports = async function() {
     useYarn: true,
     scenarios: [
       {
+        name: 'ember-3.10',
+        npm: {
+          devDependencies: {
+            'ember-source': '~3.10.0'
+          }
+        }
+      },
+      {
         name: 'ember-lts-3.12',
         npm: {
           devDependencies: {
@@ -15,37 +23,37 @@ module.exports = async function() {
         }
       },
       {
-        name: 'ember-lts-3.16',
+        name: 'ember-lts-3.14',
         npm: {
           devDependencies: {
-            'ember-source': '~3.16.0'
+            'ember-source': '~3.14.0'
           }
         }
       },
-      {
-        name: 'ember-release',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('release')
-          }
-        }
-      },
-      {
-        name: 'ember-beta',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('beta')
-          }
-        }
-      },
-      {
-        name: 'ember-canary',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('canary')
-          }
-        }
-      },
+      // {
+      //   name: 'ember-release',
+      //   npm: {
+      //     devDependencies: {
+      //       'ember-source': await getChannelURL('release')
+      //     }
+      //   }
+      // },
+      // {
+      //   name: 'ember-beta',
+      //   npm: {
+      //     devDependencies: {
+      //       'ember-source': await getChannelURL('beta')
+      //     }
+      //   }
+      // },
+      // {
+      //   name: 'ember-canary',
+      //   npm: {
+      //     devDependencies: {
+      //       'ember-source': await getChannelURL('canary')
+      //     }
+      //   }
+      // },
       // The default `.travis.yml` runs this scenario via `yarn test`,
       // not via `ember try`. It's still included here so that running
       // `ember try:each` manually or from a customized CI config will run it
